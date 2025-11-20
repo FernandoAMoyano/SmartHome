8e18dc2ef549ef100e6392aee56274be7373792c
FernandoAMoyano <fernandomoyano21@gmail.com>
2025-11-19 23:11:06 -0300

    feat/ui/init

---
+++ ui/__init__.py
@@ -0,0 +1,13 @@
+"""
+Capa de Presentación (UI Layer).
+
+Esta capa contiene toda la interfaz de usuario por consola,
+separada completamente de la lógica de negocio.
+
+Módulos:
+- console_ui: Interfaz de usuario por consola
+"""
+
+from .console_ui import ConsoleUI
+
+__all__ = ['ConsoleUI']
